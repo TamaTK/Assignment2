3c2d34b96e6629db9a6bcec14b3bfe3ef280525e
Tamahou Scott <73814295+TamaTK@users.noreply.github.com>
2023-10-22 15:15:05 +1000

    create group

--- src/app/group-management/group-management.component.ts
+++ src/app/group-management/group-management.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-group-management',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./group-management.component.css']
 })
 export class GroupManagementComponent {
+  groupName: string = '';
 
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+      const userId = 'YOUR_USER_ID'; // Retrieve this from your authentication logic or session
+      this.http.post('/group/create-group', { name: this.groupName, userId: userId })
+          .subscribe(
+              response => {
+                  console.log(response);
+                  alert('Group created successfully!');
+              },
+              error => {
+                  console.error(error);
+                  alert('Failed to create group.');
+              }
+          );
+  }
 }

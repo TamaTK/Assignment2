de567bb6180cd205accf337b379337e8feb3e9b4
Tamahou Scott <73814295+TamaTK@users.noreply.github.com>
2023-10-22 22:31:52 +1000

    add listeners

---
+++ assignment-server/sockets/listeners.js
@@ -0,0 +1,27 @@
+const addListeners = (io, socket) => {
+    console.log('New connection');
+
+    socket.on('join', (group) => {
+        console.log(`User has joined: ${group}`);
+        socket.join(group);
+    });
+
+    socket.on('sendImage', (base64Image, group) => {
+        io.to(group).emit('recieveImage', base64Image);
+    });
+
+    socket.on('sendMessage', (message, group) => {
+        io.to(group).emit('newMessage', message);
+    });
+
+    socket.on('leave', (group) => {
+        console.log(`User left group: ${group}`);
+        socket.leave(group);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User has disconnected');
+    });
+};
+
+module.exports = addListeners;

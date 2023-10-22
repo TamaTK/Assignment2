32b6176da7d2d18838f1a668d8f762c5c7de5da1
Tamahou Scott <73814295+TamaTK@users.noreply.github.com>
2023-10-22 15:20:41 +1000

    getUserID route made

--- src/app/group-management/group-management.component.ts
+++ src/app/group-management/group-management.component.ts
@@ -12,17 +12,30 @@ export class GroupManagementComponent {
   constructor(private http: HttpClient) { }
 
   onSubmit() {
-      const userId = 'YOUR_USER_ID'; // Retrieve this from your authentication logic or session
-      this.http.post('/group/create-group', { name: this.groupName, userId: userId })
-          .subscribe(
-              response => {
-                  console.log(response);
-                  alert('Group created successfully!');
-              },
-              error => {
-                  console.error(error);
-                  alert('Failed to create group.');
-              }
-          );
+    const loggedInUsername = localStorage.getItem('loggedInUser');
+    if (loggedInUsername) {
+        // Fetch the user ID from the backend using the username
+        this.http.get(`/get-user-id/${loggedInUsername}`).subscribe({
+            next: (response: any) => {
+                const userId = response.userId;
+                this.http.post('/group/create-group', { name: this.groupName, userId: userId })
+                    .subscribe({
+                        next: (response) => {
+                            console.log(response);
+                            alert('Group created successfully!');
+                        },
+                        error: (error) => {
+                            console.error(error);
+                            alert('Failed to create group.');
+                        }
+                    });
+            },
+            error: (error) => {
+                console.error('Failed to fetch user ID:', error);
+            }
+        });
+    } else {
+        alert('User not logged in.');
+    }
   }
 }

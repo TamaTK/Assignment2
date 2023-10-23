6fb41faf9e07d5bb993d42b21860229adecd54a9
Tamahou Scott <73814295+TamaTK@users.noreply.github.com>
2023-10-23 11:27:20 +1000

    Still fixing tests

--- src/app/group-management/group-management.component.spec.ts
+++ src/app/group-management/group-management.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing'; // Add this line
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GroupManagementComponent } from './group-management.component';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('GroupManagementComponent', () => {
   let component: GroupManagementComponent;
@@ -9,7 +11,8 @@ describe('GroupManagementComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [GroupManagementComponent],
-      imports: [HttpClientTestingModule] // Add this line
+      imports: [[HttpClientTestingModule],
+      [FormsModule], [RouterTestingModule]],
     });
     fixture = TestBed.createComponent(GroupManagementComponent);
     component = fixture.componentInstance;
